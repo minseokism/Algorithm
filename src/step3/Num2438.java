d261a5917b6e3fd1cddac33da9dba18e21e9618a
Minseok Lee <lagender@gmail.com>
2016-01-27 16:18:01 +0900

    Algorithm set

---
+++ src/step3/Num2438.java
@@ -0,0 +1,22 @@
+/*
+	[2438 문제] 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+	입력 - 첫째 줄에 N (1<=N<=100)이 주어진다.
+	출력 - 첫째 줄부터 N번째 줄 까지 차례대로 별을 출력한다.
+	예제 입력 - 5
+	예제 출력 -	*
+			**
+			***
+			****
+			*****	
+*/
+
+package step3;
+
+public class Num2438 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
