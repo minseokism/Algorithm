b05b83f3f826cde1ab3e367069048d0fbc62f704
Minseok Lee <lagender@gmail.com>
2016-01-29 16:50:07 +0900

    푸는중

--- src/step3/Num2742.java
+++ src/step3/Num2742.java
@@ -12,11 +12,17 @@
 
 package step3;
 
+import java.util.Scanner;
+
 public class Num2742 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a=sc.nextInt();
+		sc.close();
+		
+		for(int i=a;i>0;i--){
+			System.out.println(i);
+		}
 	}
-
 }
---
+++ src/step3/Num8393.java
@@ -0,0 +1,18 @@
+/*
+	[8393 문제] n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+	입력 - 첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+	출력 - 1부터 n까지 합을 출력한다.
+	예제 입력 - 3	
+	예제 출력 - 6
+*/
+
+package step3;
+
+public class Num8393 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
