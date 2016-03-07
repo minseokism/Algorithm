b1b2d6e0eb62c415f68ff7fce356d5995b20b0d2
Minseok Lee <lagender@gmail.com>
2016-03-07 22:45:26 +0900

    1676 완료

--- baekjoonJudge/Num1676.java
+++ baekjoonJudge/Num1676.java
@@ -7,13 +7,21 @@
 	예제 출력  - 2
 */
 
+// 풀이 5배수 일때마다 0 하나씩늘어나고 
+// 5^n 은 두번씩곱해지니 0이 하나씩 더 늘어난다
+
 package baekjoonJudge;
+
+import java.util.Scanner;
 
 public class Num1676 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		long input = sc.nextLong();
+		sc.close();
+		
+		System.out.println(input/5+input/25+input/125);
+		
 	}
-
 }
