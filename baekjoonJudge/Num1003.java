0875b16e3b5c1c4f19e3adde130b7957993b9683
Minseok Lee <lagender@gmail.com>
2016-09-01 15:41:15 +0900

    1003 문제 해결

--- baekjoonJudge/Num1003.java
+++ baekjoonJudge/Num1003.java
@@ -35,11 +35,34 @@
 */
 package baekjoonJudge;
 
+import java.util.Scanner;
+
 public class Num1003 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		for(int i=0 ; i<input ; i++) {
+			int num = sc.nextInt();
+			if(num==0) {
+				System.out.println("1 0");
+			} else {
+				System.out.println(fibo(num-1)+" "+fibo(num));
+			}
+		}
+		
+		sc.close();
+	}
+	
+	public static int fibo(int n) {
+		if (n==0) {
+			return 0;
+		} else if (n==1) {
+			return 1;
+		} else {
+			return fibo(n-1)+fibo(n-2);
+		}
 	}
 
 }
