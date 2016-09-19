e792260cf792cd3c4c430cb480718ea04ef0af1e
Minseok Lee <lagender@gmail.com>
2016-09-19 15:13:16 +0900

    2446 문제 해결 (Num2446 -> Num2443 이름 변경)

--- baekjoonJudge/Num2446.java
+++ baekjoonJudge/Num2446.java
@@ -1,15 +1,18 @@
 /*
-	[2446 문제] 별찍기 - 6
-	문제 - 첫째 줄에는 별 2*N-1개, 둘째 줄에는 별 2*N-3개, ..., N번째 줄에는 별 1개를 찍는 문제
-		   별은 가운대를 기준으로 대칭이어야 한다.
+	[2446 문제] 별찍기 - 9
+	문제 - 예제를 보고 별찍는 규칙을 유추한 뒤에 별을 찍어 보세요.
 	입력 - 첫째 줄에 N (1<=N<=100)이 주어진다.
-	출력 - 첫째 줄부터 N번째 줄 까지 차례대로 별을 출력한다.
+	출력 - 첫째 줄부터 2*N-1번째 줄 까지 차례대로 별을 출력한다.
 	예제 입력  - 5
 	예제 출력  - *********
 			  *******
 			   *****
 			    ***
 			     *
+			    ***
+			   *****
+			  *******
+		  	 *********
 */
 
 package baekjoonJudge;
@@ -21,22 +24,26 @@ public class Num2446 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int input = sc.nextInt();
-		String empty ="";
-		for(int i=0 ; i<input ; i++) {
-			empty+=" ";
-		}
+		int doubleInput = input*2;
 		
-		String star="";	
-		for(int i=0 ; i<2*input ; i++) {
+		String star ="";
+		for(int i=0 ; i<doubleInput-1 ; i++) {
 			star+="*";
 		}
 		
 		for(int i=0 ; i<input ; i++) {
-			System.out.print(empty.substring(input-i));
-			System.out.print(star.substring(2*i+1));
-			System.out.println();
+			for(int j=0 ; j<i ; j++) {
+				System.out.print(" ");
+			}
+			System.out.println(star.substring(i*2));
 		}
 		
+		for(int i=input-1 ; i>0 ; i--) {
+			for(int j=0 ; j<i-1 ; j++) {
+				System.out.print(" ");
+			}			
+			System.out.println(star.substring((i-1)*2));
+		}	
 		sc.close();
 	}
 
