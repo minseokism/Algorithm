6a9dea4964ad4e58d92fa8329d6a1f9e8eff5260
Minseok Lee <lagender@gmail.com>
2016-10-18 12:00:48 +0900

    1912 문제 해결

--- baekjoonJudge/Num1912.java
+++ baekjoonJudge/Num1912.java
@@ -1,5 +1,5 @@
 /*
-	[9012 문제] 괄호
+	[1912 문제] 연속합
 	문제 - n개의 정수로 이루어진 임의의 수열이 주어진다. 우리는 이 중 연속된 몇 개의 숫자를 선택해서 구할 수 있는 합 중 가장 큰 합을 구하려고 한다. 
 		  단, 숫자는 한 개 이상 선택해야 한다.
 		  예를 들어서 10, -4, 3, 1, 5, 6, -35, 12, 21, -1 이라는 수열이 주어졌다고 하자. 
@@ -13,10 +13,37 @@
 
 package baekjoonJudge;
 
+import java.util.Scanner;
+
 public class Num1912 {
-
 	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		int input = sc.nextInt();
+		int[] arr = new int[input];
 
+		for(int i = 0 ; i < input ; i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(go(arr));
+		sc.close();
+		
 	}
-
+	
+	public static int go(int[] arr) {
+		int max=arr[0];
+		for(int i=0 ; i<arr.length ; i++) {
+			int temp=arr[i];
+			int temp2=arr[i];
+			for(int j=i+1 ; j<arr.length ; j++) {
+				temp2+=arr[j];
+				if(temp<temp2) {
+					temp=temp2;
+				}
+			}
+			if(max < temp) {
+				max = temp;
+			}
+		}		
+		return max;
+	}
 }
