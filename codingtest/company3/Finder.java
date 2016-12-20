9d1e7ceebf1e62e7bd16d261ebcc123d82850ef5
Minseok Lee <lagender@gmail.com>
2016-12-20 10:00:28 +0900

    Refactoring codingtest3-company3

--- codingtest/company3/Finder.java
+++ codingtest/company3/Finder.java
@@ -1,27 +1,29 @@
-package main.java;
+package codingtest.company3;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
-public class Finder {
-	public List<String> nameList;
-	
-	public void find() {
-		nameList = stringToList("이유덕,이재영,권종표,이재영,박민호,강상희,이재영,김지완,최승혁,이성연,박영서,박민호,전경헌,송정환,김재성,이유덕,전경헌");
+class Finder {
+	private List<String> nameList;
+
+	void find() {
+		String names = "이유덕,이재영,권종표,이재영,박민호,강상희,이재영,김지완,최승혁,이성연,박영서,박민호,전경헌,송정환,김재성,이유덕,전경헌";
+		nameList = stringToList(names);
 		StringBuffer output = new StringBuffer();
 		
 		output.append("1. 김씨와 이씨는 각각 몇 명 인가요?\n");
-		output.append("김씨는"+findNumberOfFirstName("김")+"명, ");
-		output.append("이씨는 "+findNumberOfFirstName("이")+"명\n");
+		output.append("김씨는").append(findNumberOfFirstName("김")).append("명, ");
+		output.append("이씨는 ").append(findNumberOfFirstName("이")).append("명\n");
 		
 		output.append("2. \"이재영\"이란 이름이 몇 번 반복되나요?\n");
-		output.append(findName("이재명")+"번\n");
+		String findName = "이재영";
+		output.append(findName(findName)).append("번\n");
 		
 		output.append("3. 중복을 제거한 이름을 출력하세요.\n");
-		deduplication(nameList);
-		output.append(nameList.toString()+"\n");
+		deduplication();
+		output.append(nameList.toString()).append("\n");
 		
 		output.append("4. 중복을 제거한 이름을 오름차순으로 정렬하여 출력하세요.\n");
 		Collections.sort(nameList);
@@ -30,17 +32,15 @@ public class Finder {
 
 	}
 	
-	public List<String> stringToList(String names) {
-		List<String> list = new ArrayList<String>();
+	private List<String> stringToList(String names) {
+		List<String> list = new ArrayList<>();
 		String[] nameArray = names.split(",");
-		for(String s : nameArray) {
-			list.add(s);
-		}		
-		
+		Collections.addAll(list, nameArray);
+
 		return list;
 	}
 	
-	public int findNumberOfFirstName(String firstName) {
+	private int findNumberOfFirstName(String firstName) {
 		int numberOfFirstName = 0;
 		for(String name : nameList) {
 			if(name.startsWith(firstName)) numberOfFirstName++;
@@ -50,7 +50,7 @@ public class Finder {
 		return numberOfFirstName;
 	}
 	
-	public int findName(String findName) {
+	private int findName(String findName) {
 		int numberOfName = 0;
 		for(String name : nameList) {
 			if(name.contentEquals(findName)) numberOfName++;
@@ -59,7 +59,7 @@ public class Finder {
 		return numberOfName;
 	}
 	
-	public void deduplication(List<String> list) {
-		nameList = new ArrayList<String>(new HashSet<>(nameList));
+	private void deduplication() {
+		nameList = new ArrayList<>(new HashSet<>(nameList));
 	}
 }
