1cfaeb84e9efb28fa12f8c603233f4f21d690cf8
Minseok Lee <lagender@gmail.com>
2016-12-21 11:29:27 +0900

    10844 [ 1,2,3 더하기 ]

---
+++ baekjoonJudge/dynamicprg/Num9095.java
@@ -0,0 +1,54 @@
+/*
+	[9095 문제] 1,2,3 더하기
+	문제 - 정수 4를 1, 2, 3의 조합으로 나타내는 방법은 총 7가지가 있다.
+
+		 1+1+1+1
+		 1+1+2
+		 1+2+1
+		 2+1+1
+		 2+2
+		 1+3
+		 3+1
+
+		  정수 n이 주어졌을 때, n을 1,2,3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+	입력 - 첫쨰 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 정수 n이 주어진다. n은 양수이며 11보다 작다.
+	출력 - 각 테스트 케이스마다, n을 1,2,3의 합으로 나타내는 방법의 수를 출력한다.
+	예제 입력  - 3
+			 4	 
+			 7
+			 10
+	예제 출력  - 7
+		 	 44
+		 	 274
+*/
+package baekjoonJudge.dynamicprg;
+
+import java.util.Scanner;
+
+public class Num9095 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int time = sc.nextInt();
+		int num ; 
+		int[] d = new int[11];
+		
+		d[0] = 1;
+		
+		for(int i = 1 ; i <= 10 ; i++) {
+			for(int j = 1 ; j <= 3 ; j++) {
+				if(i-j >= 0) {
+					d[i] += d[i-j];
+				}
+			}
+		}
+		
+		while(time-- > 0) {
+			num = sc.nextInt();
+			System.out.println(d[num]);
+		}
+		
+		sc.close();
+	}
+
+}
