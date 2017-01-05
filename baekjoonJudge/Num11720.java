075dd574ec7df75081f32bfc701687eb4bf8f3ee
Minseok Lee <lagender@gmail.com>
2017-01-05 16:20:34 +0900

    11720 [ 숫자의 합 ]

---
+++ baekjoonJudge/Num11720.java
@@ -0,0 +1,32 @@
+/*
+	[11720 문제] 숫자의 합
+	문제 - N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+	입력 - 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+	출력 - 입력으로 주어진 숫자 N개의 합을 출력한다.
+	예제 입력 1 - 1
+				 1
+	예제 출력 1 - 1
+	예제 입력 2 - 5
+				 12345
+	예제 출력 2 - 15
+*/
+
+package baekjoonJudge;
+
+import java.util.Scanner;
+
+public class Num11720 {
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		String numString = sc.next();
+		int output = 0;
+
+		for(int i = 0 ; i < size ; i++) {
+			output += numString.charAt(i)-48;
+		}
+
+		System.out.println(output);
+	}
+}
